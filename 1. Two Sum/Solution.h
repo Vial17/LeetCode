fd7b5ef9f1afca86a3390917a72920773c0355f3
Vial17 <taowj17@163.com>
2015-06-28 16:48:22 +0800

    Two Sum

--- 1. Two Sum/Solution.h
+++ 1. Two Sum/Solution.h
@@ -4,9 +4,10 @@ public:
         map<int, int> val_key;
         vector<int> result(2, -1);
 	for(int i = 0; i < numbers.size(); ++i) {
+            // cur_val + exp_val = target
             int cur_val = numbers[i];
             int exp_val = target - numbers[i];
-			
+	    
             if(val_key.find(exp_val) != val_key.end()) {
                 result[0] = val_key[exp_val] + 1;
                 result[1] = i + 1;
