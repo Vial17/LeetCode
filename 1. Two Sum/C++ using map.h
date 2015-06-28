a80076c9c64c225a7bc14b2a59df9bad242f1c79
Vial17 <taowj17@163.com>
2015-06-28 15:57:00 +0800

    Two Sum

--- 1. Two Sum/C++ using map.h
+++ 1. Two Sum/C++ using map.h
@@ -2,19 +2,19 @@ class Solution {
 public:
     vector<int> twoSum(vector<int> &numbers, int target) {
         map<int, int> val_key;
-		vector<int> result(2, -1);
-		for(int i = 0; i < numbers.size(); ++i) {
-			int cur_val = numbers[i];
-			int exp_val = target - numbers[i];
+        vector<int> result(2, -1);
+	for(int i = 0; i < numbers.size(); ++i) {
+            int cur_val = numbers[i];
+            int exp_val = target - numbers[i];
 			
-			if(val_key.find(exp_val) != val_key.end()) {
-				result[0] = val_key[exp_val] + 1;
-				result[1] = i + 1;
-				break;
-			}
-			else
-				val_key[cur_val] = i;
-		}	
-		return result;
+            if(val_key.find(exp_val) != val_key.end()) {
+                result[0] = val_key[exp_val] + 1;
+                result[1] = i + 1;
+                break;
+            }
+            else
+                val_key[cur_val] = i;
+        }
+        return result;
     }
 };
