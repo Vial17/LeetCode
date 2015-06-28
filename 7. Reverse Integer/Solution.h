9b992ee863e33c1c9237a518e3d9b2ff71dcd434
Vial17 <taowj17@163.com>
2015-06-29 02:32:42 +0800

    Reverse Integer

--- 7. Reverse Integer/Solution.h
+++ 7. Reverse Integer/Solution.h
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int reverse(int x) {
-		// abs(INT_MIN) = INT_MIN will lead to infinite loop.
+        // abs(INT_MIN) = INT_MIN will lead to infinite loop.
         if(x == INT_MIN)
             return 0;
         if (x < 0)
             return -reverse(-x);
         
-		long result = 0
+        long result = 0
         while(x) {
             result = result * 10 + x % 10;
             x /= 10;
         }
 		
-		// Check whether overflow occurs.
+        // Check whether overflow occurs.
         return result > INT_MAX? 0 : result;
     }
 };
