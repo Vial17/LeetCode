74444be4b88522b10a45302b81f401e2c4cba49e
Vial17 <taowj17@163.com>
2015-06-30 16:57:26 +0800

    Valid Parentheses

---
+++ 20. Valid Parentheses/Solution.h
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> par_stk;
+        for(int i = 0; i < s.size(); ++i) {
+            switch(s[i]) {
+				case '(': {
+					par_stk.push(')');
+					break;
+				}
+				case '[': {
+					par_stk.push(']');
+					break;
+				}
+				case '{': {
+					par_stk.push('}');
+					break;
+				}
+				case ')': {
+					if(par_stk.empty() || par_stk.top() != ')')
+						return false;
+					else
+						par_stk.pop();
+					break;
+				}
+				case ']': {
+					if(par_stk.empty() || par_stk.top() != ']')
+						return false;
+					else
+						par_stk.pop();
+					break;
+				}
+				case '}': {
+					if(par_stk.empty() || par_stk.top() != '}')
+						return false;
+					else
+						par_stk.pop();
+					break;
+				}
+			}
+        }
+        return par_stk.empty();
+    }
+};
